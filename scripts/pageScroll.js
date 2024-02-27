e32b1c1eddec033b45df8f97304eb14587324129
Gabriel Jankowiec <ghjankowiec@gmail.com>
2024-02-27 17:09:51 +0100

    ADD: percentage scroller(html, css and js)

---
+++ scripts/pageScroll.js
@@ -0,0 +1,17 @@
+const bodyHeight = document.body.offsetHeight;
+const windowHeight = window.innerHeight;
+const totalToScroll = bodyHeight - windowHeight;
+
+
+function myPercentageScroll() {
+    let pageScroll = document.getElementById('pageScroll');
+    let scroll = (window.scrollY / totalToScroll) * 100;
+    let scrollPercent = scroll.toFixed(0);
+
+    if(scrollPercent == 0) {
+        pageScroll.style.display = 'none';
+    } else {
+        pageScroll.style.display = 'block';
+        pageScroll.style.width = `${scrollPercent}%`;
+    }
+}
